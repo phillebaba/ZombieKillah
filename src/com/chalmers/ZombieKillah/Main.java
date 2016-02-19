63f0cbc9b03ea78b519164ddb85a95dfec36bb85
Philip Laine <philip.laine@gmail.com>
2016-02-19 16:26:53 +0100

    Add main controller instantiation

--- src/com/chalmers/ZombieKillah/Main.java
+++ src/com/chalmers/ZombieKillah/Main.java
@@ -3,6 +3,7 @@ package com.chalmers.ZombieKillah;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Controller controller = new Controller(new ZombieKillah());
+        controller.start();
     }
 }
