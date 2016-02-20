d26e5d94d6ae5956c34113e8c4da064053451eb4
Sebastian Lind <Sebastian.E.Lind@gmail.com>
2016-02-20 20:14:05 +0100

    Started on a few classes

--- src/com/chalmers/ZombieKillah/GameObject.java
+++ src/com/chalmers/ZombieKillah/GameObject.java
@@ -1,7 +1,37 @@
 package com.chalmers.ZombieKillah;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Philip Laine on 20/02/16.
  */
 public class GameObject {
+    protected Direction direction;
+    protected int position;
+    protected boolean collideable;
+    protected Image image;
+
+
+     public GameObject(Direction direction, int position, boolean collideable, Image image){
+
+        this.direction = direction;
+        this.position = position;
+        this.collideable = collideable;
+        this.image = image;
+
+    }
+
+    enum Direction
+    {
+        EAST,
+        WEST,
+        NORTH,
+        NORTHWEST,
+        NORTHEAST,
+        SOUTH,
+        SOUTHWEST,
+        SOUTHEAST
+    }
+
 }
--- src/com/chalmers/ZombieKillah/MovableObject.java
+++ src/com/chalmers/ZombieKillah/MovableObject.java
@@ -1,7 +1,23 @@
 package com.chalmers.ZombieKillah;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Philip Laine on 20/02/16.
  */
-public class MovableObject {
+public class MovableObject extends GameObject {
+    protected float speed;
+
+    public MovableObject (Direction direction, int position, boolean collideable, Image image, float speed){
+            super(direction, position, collideable, image);
+            this.speed = speed;
+    }
+
+    protected void step(){
+
+
+
+
+    }
 }
