be1d13d8d5c1cd79b6ab993e48bf79640f877a71
BuildTools <danniwezz@hotmail.com>
2016-02-23 15:12:06 +0100

    ImageLoader added to GameObject.

--- src/com/chalmers/ZombieKillah/GameObject.java
+++ src/com/chalmers/ZombieKillah/GameObject.java
@@ -4,4 +4,16 @@ package com.chalmers.ZombieKillah;
  * Created by Philip Laine on 20/02/16.
  */
 public class GameObject {
+    private BufferedImage image;
+
+    public GameObject(String path) {
+        try {
+            image = ImageIO.read(ImageLoader.class.getResource(path));
+            catch(IOException e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+        }
+    }
 }
