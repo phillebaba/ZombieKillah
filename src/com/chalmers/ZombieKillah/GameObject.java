8d50665058d426e6c00c949696d82c722246b74a
BuildTools <danniwezz@hotmail.com>
2016-02-23 15:02:30 +0100

    Image method done in GameObject.

--- src/com/chalmers/ZombieKillah/GameObject.java
+++ src/com/chalmers/ZombieKillah/GameObject.java
@@ -1,7 +1,34 @@
 package com.chalmers.ZombieKillah;
 
+import com.sun.deploy.ui.ImageLoader;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 /**
  * Created by Philip Laine on 20/02/16.
  */
 public class GameObject {
+
+    private BufferedImage image;
+
+public GameObject(String path){
+        try {
+            image = ImageIO.read(ImageLoader.class.getResource(path));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+}
+
+
+
+
+
+
 }
