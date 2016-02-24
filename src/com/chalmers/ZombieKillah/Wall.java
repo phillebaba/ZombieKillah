85918b6907712f9e49523abd434251054c05e573
Sebastian Lind <Sebastian.e.lind@gmail.com>
2016-02-24 15:42:33 +0100

    Added class Wall

---
+++ src/com/chalmers/ZombieKillah/Wall.java
@@ -0,0 +1,18 @@
+package com.chalmers.ZombieKillah;
+
+import javax.imageio.ImageIO;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Sebastian Lind on 2016-02-24.
+ */
+public class Wall extends GameObject {
+
+    public Wall (String path, double x, double y){
+        super("Wall",x,y);
+    }
+
+}
