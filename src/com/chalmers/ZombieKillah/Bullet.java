2377102032c5a70ec900570ff6c9ead14a126b2d
Jesper Rask <raggy_ragge@hotmail.com>
2016-02-24 17:11:39 +0100

    Fix bullet class

--- src/com/chalmers/ZombieKillah/Bullet.java
+++ src/com/chalmers/ZombieKillah/Bullet.java
@@ -1,17 +1,13 @@
 package com.chalmers.ZombieKillah;
 
 /**
- * Created by Jeppe on 2016-02-24.
+ * Created by Jesper Rask on 2016-02-24.
  */
 public class Bullet extends MovableObject {
 
     public Bullet(Direction direction,int x, int y){
         super("Bullet",x,y);
+        this.speed = 0.5;
+        this.direction = direction;
     }
-
-
-
-
-
-    
 }
