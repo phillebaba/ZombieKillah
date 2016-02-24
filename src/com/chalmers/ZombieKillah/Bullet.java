1855c242cfb9ade985bf6765d7062ff014a53c02
Jesper Rask <raggy_ragge@hotmail.com>
2016-02-24 16:54:47 +0100

    Create bullet class

---
+++ src/com/chalmers/ZombieKillah/Bullet.java
@@ -0,0 +1,17 @@
+package com.chalmers.ZombieKillah;
+
+/**
+ * Created by Jeppe on 2016-02-24.
+ */
+public class Bullet extends MovableObject {
+
+    public Bullet(Direction direction,int x, int y){
+        super("Bullet",x,y);
+    }
+
+
+
+
+
+    
+}
