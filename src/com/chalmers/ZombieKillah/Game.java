378d86de66c76e1e8f9dfa7ac57b498ba1d002cb
Philip Laine <philip.laine@gmail.com>
2016-02-24 15:23:58 +0100

    Add basic collisions detection

--- src/com/chalmers/ZombieKillah/Game.java
+++ src/com/chalmers/ZombieKillah/Game.java
@@ -14,15 +14,25 @@ public abstract class Game {
     protected abstract void objectsDidCollide(GameObject object1, GameObject object2);
 
     public Game() {
-        this.player = new Player();
+        this.player = new Player(10, 10);
         this.objects = new ArrayList<GameObject>();
     }
 
     public void checkCollisions() {
-
+        for (GameObject object1: objects) {
+            if (object1.collidable) {
+                for (GameObject object2 : objects) {
+                    if (object2.collidable && object1.frame.intersects(object2.getFrame())) {
+                        System.out.println("collision");
+                    }
+                }
+            }
+        }
     }
 
     public ArrayList<GameObject> getObjects() {
         return objects;
     }
+
+
 }
