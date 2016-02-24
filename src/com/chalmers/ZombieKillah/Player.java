cd25beca54ad52e90f52649bb98a524cbd2c4ea8
Daniel Posch <danniwezz@hotmail.com>
2016-02-24 14:00:24 +0100

    Turn(Input input) method done for testing.

--- src/com/chalmers/ZombieKillah/Player.java
+++ src/com/chalmers/ZombieKillah/Player.java
@@ -1,7 +1,40 @@
 package com.chalmers.ZombieKillah;
 
 /**
- * Created by Philip Laine on 20/02/16.
+ * Created by Daniel Posch on 20/02/24.
  */
 public class Player {
+
+    public GameObject.Direction turn(Input input){
+
+        switch (input){
+            case VK_LEFT:
+                MovableObject.step(WEST);
+                break;
+            case VK_DOWN:
+                MovableObject.step(SOUTH);
+                break;
+            case VK_UP:
+                MovableObject.step(NORTH);
+                break;
+            case VK_RIGHT:
+                MovableObject.step(EAST);
+                break;
+            case VK_RIGHT|VK_UP:
+                MovableObject.step(NORTHEAST);
+                break;
+            case VK_LEFT|VK_UP:
+                MovableObject.step(NORTHWEST);
+                break;
+            case VK_RIGHT|VK_DOWN:
+                MovableObject.step(SOUTHEAST);
+                break;
+            case VK_LEFT|VK_DOWN:
+                MovableObject.step(SOUTHWEST);
+                break;
+    }
+
+
+
+    }
 }
