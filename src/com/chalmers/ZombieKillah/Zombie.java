58e67a00f54de7914972832ff09055ff07eec0d1
Jesper Rask <raggy_ragge@hotmail.com>
2016-02-24 15:30:39 +0100

    Fix Zombie move method

--- src/com/chalmers/ZombieKillah/Zombie.java
+++ src/com/chalmers/ZombieKillah/Zombie.java
@@ -3,17 +3,36 @@ package com.chalmers.ZombieKillah;
 import java.awt.geom.Rectangle2D;
 
 /**
- * Created by Philip Laine on 20/02/16.
+ * Created by Jesper Rask on 20/02/16.
  */
-public class Zombie {
+public class Zombie extends Character {
 
-    public void move(Rectangle2D.Double frame){
-
-        super.step()
-
-
+    public Zombie(int x, int y){
+        super("Zombie", x, y);
     }
 
+    public void move(Rectangle2D.Double destination){
 
-
+        double angle = Math.toDegrees(Math.atan2(destination.getX() - frame.getX(), destination.getY() - frame.getY()));
+        angle = angle + Math.ceil( -angle / 360 ) * 360;
+        if(angle >= 337.5 && angle < 22.5) {
+           direction = Direction.WEST;
+        } else if(angle >= 22.5 && angle < 66.5) {
+            direction = Direction.NORTHEAST;
+        } else if(angle >= 66.5 && angle < 112.5) {
+            direction = Direction.NORTH;
+        } else if(angle >= 112.5 && angle < 157.5) {
+            direction = Direction.NORTHWEST;
+        } else if(angle >= 157.5 && angle < 202.5) {
+            direction = Direction.WEST;
+        } else if(angle >= 202.5 && angle < 247.5) {
+            direction = Direction.SOUTHWEST;
+        } else if(angle >= 247.5 && angle < 292.5) {
+            direction = Direction.SOUTH;
+        } else if (angle >= 292.5 && angle < 337.5) {
+            direction = Direction.SOUTHEAST;
+        }
+        
+        super.step();
+    }
 }
