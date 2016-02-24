c882b0b8346228427f5ccde916b0cabc3181176e
Jesper Rask <raggy_ragge@hotmail.com>
2016-02-24 13:54:06 +0100

    Namechange on Weapon class and setup on the Zombieclass

--- src/com/chalmers/ZombieKillah/Weapon.java
+++ src/com/chalmers/ZombieKillah/Weapon.java
@@ -3,11 +3,11 @@ package com.chalmers.ZombieKillah;
 /**
  * Created by Sebastian Lind on 2016-02-23.
  */
-public class Weapond extends GameObject {
+public class Weapon extends GameObject {
 
     private int damage;
 
-    public Weapond (String path, int posX, int posY){
+    public Weapon(String path, int posX, int posY){
         super(path, posX, posY);
         this.collidable = false;
         this.damage = 50;
--- src/com/chalmers/ZombieKillah/Zombie.java
+++ src/com/chalmers/ZombieKillah/Zombie.java
@@ -1,7 +1,19 @@
 package com.chalmers.ZombieKillah;
 
+import java.awt.geom.Rectangle2D;
+
 /**
  * Created by Philip Laine on 20/02/16.
  */
 public class Zombie {
+
+    public void move(Rectangle2D.Double frame){
+
+        super.step()
+
+
+    }
+
+
+
 }
