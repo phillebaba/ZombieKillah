4bd64f8dcb8e2fd2a52644cbadbcbb329daa575e
Sebastian Lind <Sebastian.e.lind@gmail.com>
2016-02-24 16:36:42 +0100

    Fixed Wall class

--- src/com/chalmers/ZombieKillah/Wall.java
+++ src/com/chalmers/ZombieKillah/Wall.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  */
 public class Wall extends GameObject {
 
-    public Wall (String path, double x, double y){
+    public Wall (double x, double y){
         super("Wall",x,y);
     }
 
