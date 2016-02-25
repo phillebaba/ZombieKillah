fc6fee3c2437e811fa393979583df7625faa1cad
Philip Laine <philip.laine@gmail.com>
2016-02-25 03:58:47 +0100

    Increase bullet speed

--- src/com/chalmers/ZombieKillah/Bullet.java
+++ src/com/chalmers/ZombieKillah/Bullet.java
@@ -7,7 +7,7 @@ public class Bullet extends MovableObject {
 
     public Bullet(Direction direction, double x, double y){
         super("Bullet", x, y);
-        this.speed = 0.5;
+        this.speed = 10;
         this.direction = direction;
     }
 }
