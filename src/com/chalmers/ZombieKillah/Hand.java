ecc6ebb026a52a0e431c32ac78dc20d7a2e6f862
Sebastian Lind <Sebastian.e.lind@gmail.com>
2016-03-06 14:46:54 +0100

    Added Java docs

--- src/com/chalmers/ZombieKillah/Hand.java
+++ src/com/chalmers/ZombieKillah/Hand.java
@@ -1,9 +1,14 @@
 package com.chalmers.ZombieKillah;
 
 /**
- * Created by Philip Laine on 03/03/16.
+ * Represent a hand weapon, is a sub class to Weapon
+ * @author Philip Laine
+ * @version 1.0.0 03/03/16
  */
 public class Hand extends Weapon {
+    /**
+     * Initializes a new type of weapon with hand characteristics
+     */
     public Hand() {
         super(0.9, 10);
     }
